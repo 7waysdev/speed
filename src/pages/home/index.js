4e639eef1b4fe6ef8e9e8559abb481c348c7a8d0
Diego Macedo <diegomacedo@mac.local>
2018-03-22 16:46:04 -0300

    fix

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,9 @@
 import { BasePage } from '../base'
-import { Product } from '../../modules/product'
 
 export class HomePage extends BasePage {
 
     init(){
-        
-        let product = new Product();
 
-        product.getProductById(3293970).then(function(res){
-
-        })
     }
 }
 
